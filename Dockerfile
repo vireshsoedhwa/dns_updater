8d3d95faa76e23b3e15eeb54dac587283e7d109d
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2022-09-24 04:20:47 -0700

    setup script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine/curl:latest
 
+RUN apk update && apk add bind-tools
+
 COPY docker-entrypoint.sh /usr/local/bin
 RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,20 +3,74 @@
 set -e
 >&2 echo "Checking address"
 
-ip=$(curl -s icanhazip.com)
+>&2 echo CURL_ENDPOINT_PRIMARY: $CURL_ENDPOINT_PRIMARY
+>&2 echo CURL_ENDPOINT_SECONDARY: $CURL_ENDPOINT_SECONDARY
+>&2 echo DIG_HOST: $DIG_HOST
 
-if expr "$ip" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
-  for i in 1 2 3 4; do
-    if [ $(echo "$ip" | cut -d. -f$i) -gt 255 ]; then
-      echo "fail ($ip)"
-      exit 1
+curl_result_primary=$(curl -s $CURL_ENDPOINT_PRIMARY)
+curl_result_secondary=$(curl -s $CURL_ENDPOINT_SECONDARY)
+
+echo "curl result primary ($curl_result_primary)"
+echo "curl result secondary ($curl_result_secondary)"
+dig_result=$(dig @8.8.8.8 +short $DIG_HOST)
+echo "dig result ($dig_result)"
+
+function validate_ip()
+{
+    if expr "$1" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*' >/dev/null; then
+    for i in 1 2 3 4; do
+        if [ $(echo "$1" | cut -d. -f$i) -gt 255 ]; then
+            echo "failed ($1)"
+            # exit 1
+            return 1
+        fi
+    done
+        echo "validated ($1)"
+        # exit 0
+        return 0
+    else
+        echo "failed validation ($1)"
+        # exit 1
+        return 1
     fi
-  done
-  echo "success ($ip)"
-  exit 0
-else
-  echo "fail ($ip)"
-  exit 1
+}
+
+function fix_ip()
+{
+    >&2 echo fixing dns ip to $1
+    echo $(curl -s -X PATCH \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $DNS_TOKEN" \
+    "https://api.digitalocean.com/v2/domains/$DOMAIN_NAME/records/$DOMAIN_RECORD_ID" \
+    --data '{
+        "data": "'$1'"
+        }')
+}
+
+if validate_ip $curl_result_primary ; 
+then 
+    if [ "$dig_result" = "$curl_result_primary" ]; then
+        echo "no fixing needed"
+        exit 0
+    else
+        fix_ip $curl_result_primary
+        exit 1
+    fi
+else 
+    echo "primary curl failed"; 
+fi
+
+if validate_ip $curl_result_secondary ; 
+then 
+    if [ "$dig_result" = "$curl_result_secondary" ]; then
+        echo "no fixing needed"
+        exit 0
+    else
+        fix_ip $curl_result_secondary
+        exit 1
+    fi
+else 
+    echo "secondary curl failed"; 
 fi
 
 # exec "$@"
