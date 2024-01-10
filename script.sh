98a339b9861ed451eeb5365210c65eecd67ada49
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2024-01-10 08:40:26 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/sh
+#!/bin/sh
 
 # Check if config file was provided
 if [ $# -eq 0 ]; then
@@ -30,20 +30,17 @@ if [ -z "${PUBLIC_IP_CHECK_SERVICE}" ] ||
     exit 1
 fi
 
-# Check if the file exists
-if [ -f public_ip.txt ]; then
-    echo "Reading public IP from file"
-    public_ip=$(cat public_ip.txt)
+if [ -f dns_record.txt ]; then
+    echo "Reading dns record IP from file"
+    dns_ip_address=$(cat dns_record.txt)
 else
-    echo "Getting public IP from service"
-    public_ip=$(curl -s $PUBLIC_IP_CHECK_SERVICE)
-    echo "$public_ip" > public_ip.txt
+    echo "Getting dns record IP"
+    dns_ip_address=$(dig +short $DOMAIN_NAME)
+    echo "$dns_ip_address" > dns_record.txt
 fi
 
-# Get the IP address of the domain name
-dns_ip_address=$(dig +short $DOMAIN_NAME)
-
-
+    echo "Getting public IP from service"
+    public_ip=$(curl -s $PUBLIC_IP_CHECK_SERVICE)
 
 function digitalocean_dns_update()
 {
@@ -57,26 +54,29 @@ function digitalocean_dns_update()
         "data": "'$1'"
         }')
     if [ "$http_code" == "200" ]; then
-        echo "$(date): DNS IP address updated successfully: $public_ip" >> log.txt
+        echo "$(date): DNS IP address updated successfully: $public_ip" | tee -a log.txt
         echo "$public_ip" > public_ip.txt
     else
-        dnsfailure_msg="DNS IP address update failed: HTTP code: $http_code"
-        echo "$(date): $dnsfailure_msg" >> log.txt
-        echo $dnsfailure_msg
+
+        echo "$(date): DNS IP address update failed: HTTP code: $http_code" | tee -a log.txt
+        tail -n 100 log.txt > temp.txt && mv temp.txt log.txt
         exit 1
     fi
 }
 
 # Check if the IP addresses match
 if [ "$dns_ip_address" == "$public_ip" ]; then
-    echo "$(date): IP addresses match...skipping update"  >> log.txt
+
+    echo "$(date): DNS IP address ($dns_ip_address) matches public IP ($public_ip)" | tee -a log.txt
+    tail -n 100 log.txt > temp.txt && mv temp.txt log.txt
     exit 0
 else
-    echo "$(date): DNS IP address ($dns_ip_address) does not match public IP ($public_ip)" >> log.txt
+    echo "$(date): DNS IP address ($dns_ip_address) does not match public IP ($public_ip)" | tee -a log.txt
     echo "Updating DNS IP address"
     digitalocean_dns_update $public_ip
-    exit 1
 fi
 
 # Keep only the last 100 lines in the log file
 tail -n 100 log.txt > temp.txt && mv temp.txt log.txt
+
+exit 1
